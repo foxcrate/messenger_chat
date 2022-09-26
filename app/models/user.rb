9cf3c1379bdd47e011813c058457a6e383180abb
foxcrate <ahmedmustafa.pro19@gmail.com>
2022-09-26 14:13:06 +0200

    finish front-end basics

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
 
     validates_uniqueness_of :username
 
+    def self.generate
+        adjectives = ['blue', 'red', 'yellow', 'black', 'green']
+        nouns = ['lion', 'fox', 'elephant', 'hippo', 'rat']
+        number = rand.to_s[2..4]
+        username = "#{adjectives.sample}_#{nouns.sample}_#{number}"
+        User.create(username: username)
+
+    end
+
 end
